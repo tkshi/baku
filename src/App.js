193c0b7fa270c902ad8acb390b21937e19f4204d
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-02-12 04:34:32 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 var request = require('superagent');
-request.get('http://bbakku.tk',(e,res,body)=>{
-  console.log(e,res)
-})
+request.get('http://www.soccermemes.net/images/loading.gif')
+const loading_image_url = 'http://www.cuisson.co.uk/templates/cuisson/supersize/slideshow/img/progress.BAK-FOURTH.gif'
+const base_url = process.env.URL || 'http://bbakku.tk'
 
 export default class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          image:'http://cdn.theatlantic.com/assets/media/interactives/2014/06/chicago/tiles/blank.png'
+      };
+  }
+  handleFileSelect = (e)=>{
+    this.setState({image:loading_image_url})
+    var formData = new FormData();
+    formData.append('file',e.target.files[0])
+    request.post(`${base_url}/image`)
+    .send(formData)
+    .end((e,res)=>{
+      this.setState({image:res.body.url})
+    })
+  };
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Hello, world.</h1>
+        <input type='file' onChange={this.handleFileSelect}></input>
+        <img src={this.state.image} />
+      </div>
+
     );
   }
 }
