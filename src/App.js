c5b63fd38136a49c27caf57ab08faa2e3e924cae
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-02-12 02:01:46 +0900

    consoleLogFile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+var request = require('superagent');
+request.get('http://bbakku.tk',(e,res,body)=>{
+  console.log(e,res)
+})
 
 export default class App extends Component {
   render() {
